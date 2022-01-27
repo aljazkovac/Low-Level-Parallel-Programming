c3011811cdcb0ca0199337459bfbfcaef9023c90
Simon Jaklovsky <sija3739@rackham2.uppmax.uu.se>
2022-01-27 10:56:12 +0100

    bash script and results

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-array=("PTHREAD" "OMP")
-echo "" > results.txt
+array=("CTHREADS" "OMP")
+#echo "" > results.txt
 for i in ${array[@]}; do
-  for ((j = 2; j <= 4; j=j+2)); do
+  for ((j = 2; j <= 16; j=j+2)); do
     echo "${i} ${j}" >> results.txt
-    demo/demo --timing-mode --implementation $i --threads $j hugeScenario.xml | grep "Speedup" | awk '{print $2}' >> text.txt
+    demo/demo --timing-mode --implementation $i --threads $j hugeScenario.xml | grep "Speedup" | awk '{print $2}' >> results.txt
   done
 done
 
